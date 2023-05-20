f696209304735fd139da37f686b515209d9ebf39
abenaderby <boatemaaharobed@gmail.com>
2023-05-20 05:53:42 -0700

    ./as

---
+++ first_learning_c/assignment.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints values between 0 and 50
+ * Return: (0) Always
+ */
+
+int main(void)
+{
+    int i = 1;
+    
+    while (i < 50)
+    {
+	printf("%d\n", i);
+	 i++;
+    }
+
+     return 0;
+}
