604211b911c61d49149b2133a9c5b6e385c10eae
abenaderby <boatemaaharobed@gmail.com>
2023-06-12 10:45:56 -0700

    hello

---
+++ first_learning_c/ako.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int agreed = 1;
+
+   if (agreed)
+   {
+	   printf("%d\n", agreed);
+       return (1);
+   }
+
+  else
+  {
+       return (0);
+  }
+}
